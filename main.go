8784ddb5276647da42c44e8d6fa88acf86af6102
Callison <Brandon.L.Callison@saic.com>
2023-08-01 21:10:08 -0500

    fix

--- main.go
+++ main.go
@@ -22,20 +22,17 @@ import (
 
 //[END imports]
 func main() {
-	log.Print("starting server...")
 	http.HandleFunc("/", handler)
 
 	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
+	port := "8080"
 	if port == "" {
 			port = "8080"
-			log.Printf("defaulting to port %s", port)
 	}
 
 	// Start HTTP server.
-	log.Printf("listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
-			log.Fatal(err)
+			log.Errorf(err)
 	}
 }
 
